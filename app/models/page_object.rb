a227f1aa797e91a5f879f0cf08d1435483db607b
Matthew Moore <matt@thrivesmart.com>
2008-12-07 11:58:11 -0800

    fixing timezone parsing issues

--- app/models/page_object.rb
+++ app/models/page_object.rb
@@ -4,16 +4,24 @@ class PageObject < ActiveRecord::Base
   alias_method :original_to_xml, :to_xml
   include ThriveSmartObjectMethods
   
-  attr_accessor :starts, :ends
+  attr_accessor :starts, :ends, :starts_at, :ends_at
   
   before_validation :parse_dates
   
   validates_presence_of :starts, :on => :update
   
   def validate
-    if starts && ends && starts_datetime > ends_datetime
-      errors.add(:ends, " must be after starting time")
+    if starts_at && ends_at && starts_datetime > ends_datetime
+      errors.add(:starts_at, " must be before end time")
     end
+  end
+  
+  def starts_at
+    @starts_at ||= starts_datetime && time_zone ? starts_datetime.in_time_zone(time_zone).strftime('%Y-%m-%d %H:%M') : starts_datetime.strftime('%Y-%m-%d %H:%M')
+  end
+  
+  def ends_at
+    @ends_at ||= ends_datetime && time_zone ? ends_datetime.in_time_zone(time_zone).strftime('%Y-%m-%d %H:%M') : ends_datetime.strftime('%Y-%m-%d %H:%M')
   end
   
   def starts
@@ -26,12 +34,12 @@ class PageObject < ActiveRecord::Base
   
   protected
     def parse_dates
-      logger.debug "PARSING DATES!--------- // STARTS : #{starts.inspect} // ENDS : #{ends.inspect}"
+      logger.debug "PARSING DATES!--------- // STARTS : #{starts_at.inspect} // ENDS : #{ends_at.inspect}"
       old_timezone = Time.zone
       Time.zone = time_zone
       Chronic.time_class = Time.zone
-      self.starts_datetime = Chronic.parse(starts)
-      self.ends_datetime = Chronic.parse(ends)
+      self.starts_datetime = Chronic.parse(starts_at)
+      self.ends_datetime = Chronic.parse(ends_at)
       Time.zone = old_timezone
       logger.debug "FINISHED PARSING DATES!--------- // STARTS : #{starts_datetime.inspect} // ENDS : #{ends_datetime.inspect}"
     end
